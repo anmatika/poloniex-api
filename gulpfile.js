f57998a61194e8930ce6a83e3b99908d1952422f
anmatika <4ntti.matikainen@gmail.com>
2017-05-31 17:32:34 +0300

    fix tests

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const mocha = require('gulp-mocha');
+const gutil = require('gulp-util');
 
 gulp.task('watch-mocha', () => {
     // run in oneshot
--- test/tradingApi.test.js
+++ test/tradingApi.test.js
@@ -10,17 +10,6 @@ const request = require('request');
 const target = require('../tradingApi.js').create('', '');
 chai.should();
 
-describe('makeRequestPublic', () => {
-  const stubYield = { body: JSON.stringify({ foo: 'bar' }) };
-  const getStub = sinon.stub(request, 'get').yields(null, null, stubYield);
-
-  it('should call request get with returnTicker', () => {
-      const expected = { body: JSON.stringify({ foo: 'bar' }) };
-      target.returnTicker().should.eventually.deep.equal(expected);
-      expect(request.get).to.have.been.calledOnce;
-  } )
-});
-
 describe('makeRequest', () => {
 
   const stubYield = { body: JSON.stringify({ foo: 'bar' }) };
